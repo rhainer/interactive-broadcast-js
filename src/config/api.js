c7381556771c6e69c68ed810e09c594a5907beae
Rob Hainer <robert@tokbox.com>
2018-09-12 11:08:20 -0700

    initial config

---
+++ src/config/api.js
@@ -0,0 +1,7 @@
+const api = {
+  localhost: 'http://localhost:3001', // Replace this url for your local API server
+  production: 'https://rh-spotlight-api.herokuapp.com:3002', // Replace this url for your production API server
+};
+
+export default api;
+ 
---
+++ src/config/firebase.js
@@ -0,0 +1,11 @@
+const firebase = {
+  apiKey: 'AIzaSyA_wDFIkC1KBTvcmk1lQFZv1zZ5vM7r5TY', // firebase apikey
+  authDomain: 'spotlight-rh.firebaseapp.com', // firebase authDomain
+  databaseURL: 'https://spotlight-rh.firebaseio.com', // firebase databaseURL
+  projectId: 'spotlight-rh', // firebase projectId
+  storageBucket: 'spotlight-rh.appspot.com', // firebase storageBucket
+  messagingSenderId: '1010820524374',  // firebase messagingSenderId
+};
+
+export default firebase;
+ 
